8c86c4a6d1dd0e3760bbc812d43940a40e3f948e
Chukwuka Okonkwo David <chukwukaokonkwo99@gmail.com>
2024-10-29 13:26:43 +0100

    added

--- src/app/birthday/page.tsx
+++ src/app/birthday/page.tsx
@@ -1,29 +1,49 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { CarouselPlugin } from '../../components/CarouselPlugin';
 
 export default function Page() {
   return (
-    <div className='body-div'>
-    <div className="birthdayCard">
-    <div className="cardFront">
-      <h3 className="happy">Happy Birthday Chi❤️</h3>
-      <div className="balloons">
-        <div className="balloon-1"></div>
-        <div className="balloon-2"></div>
-        <div className="balloon-3"></div>
-        <div className="balloon-4"></div>
+    <div className='body-div mt-36'>
+      <div className="birthdayCard">
+        <div className="cardFront">
+          <h3 className="happy">Happy Birthday Chi ❤️</h3>
+          <div className="balloons">
+            <div className="balloon-1"></div>
+            <div className="balloon-2"></div>
+            <div className="balloon-3"></div>
+            <div className="balloon-4"></div>
+          </div>
+        </div>
+        <div className="cardInside">
+          <h3 className="back">HAPPY BIRTHDAY</h3>
+          <p className="name-p">Ore mi,</p>
+          <p className="name-p">
+            To you, I wish the best of days ahead. Nothing's going to stop you cause you're set. Happy birthday my love
+          </p>
+          <p className="name">Chuka</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center justify-center mt-24 gap-10"> {/* Center content */}
+        {/* Carousel */}
+        <div className="flex justify-center mr-0 md:mr-52">
+          <CarouselPlugin />
+        </div>
+
+        {/* Popover */}
+        <div className="flex justify-center">
+          <Popover>
+            <PopoverTrigger className="mb-32 md:mb-0">Open</PopoverTrigger>
+            <PopoverContent>Place content for the popover here.</PopoverContent>
+          </Popover>
+        </div>
       </div>
     </div>
-    <div className="cardInside">
-      <h3 className="back">HAPPY BIRTHDAY</h3>
-      <p>Dear Friend,</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-        expedita debitis esse quaerat adipisci iste illum placeat incidunt
-        reprehenderit laudantium!
-      </p>
-      <p className="name">xxx</p>
-    </div>
-  </div>
-  </div>
-  )
+  );
 }
---
+++ src/components/CarouselPlugin.tsx
@@ -0,0 +1,58 @@
+"use client"
+import React from "react"
+import Autoplay from "embla-carousel-autoplay"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image"
+import chi from '../assets/chi.jpg'
+import chi2 from '../assets/chi2.jpg';
+
+
+
+
+export function CarouselPlugin() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
+
+    // Array of images
+    const images = [chi, chi2];
+
+  return (
+    <div className=" max-w-[200px] mb-24"
+    >
+      <h1 className="text-center">Birthday Girl</h1>
+      <Carousel
+        plugins={[plugin.current]}
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent>
+        {images.map((image, index) => (
+            <CarouselItem key={index}>
+              <div>
+                <Card>
+                  <CardContent className="">
+                    <Image
+                    src={image}
+                    className=""
+                    />
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  )
+}
+
